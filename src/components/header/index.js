809d4c03807c4474e5a021fc2a33d0585e2fcefb
shaper <15519586771@163.com>
2021-05-31 11:12:24 +0800

    chore: init

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -26,12 +26,12 @@ export default function Header() {
         <span className="text">
           前端学习路线
           <span className="emoji">
-            <span role="img" aria-label="line">🧶</span>
-            <span role="img" aria-label="road">🦌</span>
+            <span role="img" aria-label="line">🌋</span>
+            <span role="img" aria-label="road">⛰️</span>
           </span>
         </span>
       </div>
-      <div className="sub-title"><span role="img" aria-label="workholic">👨🏻‍💻</span> 好好学习，天天敲代码 <span role="img" aria-label="workholic">👨🏻‍💻</span></div>
+      <div className="sub-title"><span role="img" aria-label="workholic">👨🏻‍💻</span> 今天不学习，明天变垃圾 <span role="img" aria-label="workholic">👨🏻‍💻</span></div>
     </div>
   );
 }
